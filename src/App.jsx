ec35c64d5198985bbd42a2870fcd62a20237d3cd
TGSsirrett31 <ssirrettcamara@gmail.com>
2024-06-04 22:48:57 -0400

    Final NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,49 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import ssLogo from './assets/Sa2.png'
 import './App.css'
 
-
 function App() {
   const [linksVisible, setLinksVisible] = useState(false);
+  const [activeLink, setActiveLink] = useState('');
+  const [logoClicked, setLogoClicked] = useState(false); // Nuevo estado para controlar si se hizo clic en el logo
 
   const toggleLinks = () => {
     setLinksVisible(!linksVisible);
+    setLogoClicked(true); // Establecemos el estado del clic en el logo como verdadero cuando se hace clic en él
+    setTimeout(() => {
+      setLogoClicked(false); // Restablecemos el estado del clic en el logo como falso después de un tiempo
+    }, 500); // Ajusta este valor para que coincida con la duración de la transición de los enlaces
+  };
+
+  const handleLinkClick = (link) => {
+    setActiveLink(link);
   };
 
   return (
     <>
-<div class="background-container">
-    <img className='bgImg' src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1231630/moon2.png" alt="" />
-    <div class="stars"></div>
-    <div class="twinkling"></div>
-    <div class="clouds"></div>
-</div>
-    <nav className="navbar">
-      <div className={`links left ${linksVisible ? 'visible' : ''}`}>
-        <a href="#home">Home</a>
-        <a href="#about">About</a>
+   <div className="background-container">
+        <img className='bgImg' src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1231630/moon2.png" alt="" />
+        <div className="stars"></div>
+        <div className="twinkling"></div>
+        <div className="clouds"></div>
       </div>
-      <div className="logo" onClick={toggleLinks}>
-        <img src={ssLogo} alt="Logo" />
-      </div>
-      <div className={`links right ${linksVisible ? 'visible' : ''}`}>
-        <a href="#services">Services</a>
-        <a href="#contact">Contact</a>
-      </div>
-    </nav>
-  </>
+      <nav className="navbar">
+        <div className={`links left ${linksVisible ? 'visible' : ''}`}>
+          <a className="nav-item link2" href="#home">Home</a>
+          <a className="nav-item link1" href="#about">About</a>
+        </div>
+        <div className="logo" onClick={toggleLinks}>
+          <img src={ssLogo} alt="Logo" className={logoClicked ? 'clicked' : ''} />
+        </div>
+        <div className={`links right ${linksVisible ? 'visible' : ''}`}>
+          <a className="nav-item link3" href="#services">Services</a>
+          <a className="nav-item link4" href="#contact">Contact</a>
+        </div>
+      </nav>
+
+      <h1>Hi</h1>  
+            <p>Buenas</p>
+    </>
   )
 }
 
---
+++ src/components/home_page.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import ssLogo from '../assets/Sa2.png'
+import '../App.css'
+
+function HomePage (){
+    return (
+        <>
+            <h1>Hi</h1>  
+            <p>Buenas</p>
+        </>
+    )
+}
+
+export default HomePage
